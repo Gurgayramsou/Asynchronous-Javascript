a39a99f7ff8bab9b3ed54bb8da18b7d50c18163a
Gurgayramsou <hegderam17@gmail.com>
2023-09-05 18:24:02 -0700

    asjx file is added

---
+++ asjx.js
@@ -0,0 +1,11 @@
+// How to do a Ajax call to get WebApi result?
+
+// Old type of  method:
+
+// By XMLHttpRequest()
+
+`const request = XMLHttpRequest();`
+
+`request.open(’GET’, ‘url’)`
+
+`request.send()`  - this should be inside the event listener in call back function
